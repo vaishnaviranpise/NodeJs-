b5df9c6ebe82f07d9e1ef3f8356ecf28f80e587c
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-02-17 01:03:33 -0800

    Add files via upload

---
+++ application.js
@@ -0,0 +1,10 @@
+var http=require('http');
+var r1=require('readline').createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+r1.question('Enter the First Name:',(name)=>{
+    r1.question('Enter the Last Name:',(lname))
+    3
+
+})
